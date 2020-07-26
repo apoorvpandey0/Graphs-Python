3cee7e3cea20da3d1900ea3f186801f39289e5dd
Apoorv Pandeym <apoorvpandey0@gmail.com>
2020-07-26 11:29:04 +0530

    Added some comments to connectedcmponents.py

--- connectedComponents_DFS.py
+++ connectedComponents_DFS.py
@@ -1,5 +1,5 @@
-#Finds the number of connected components in a graph using BFS algorithm
-# Connected components means number of isolated components or groups of components
+# Finds the number of connected components in a graph using BFS algorithm
+# Connected components means number of isolated components or islands in a graph
 
 a = {  0:[1,9],
        1:[0,8],
@@ -19,19 +19,20 @@ visited = [False for i in range(n)]
 def findComponents():
     components = [None for i in range(n)]
     count = 0
-    #Loop over each Node
-    for node in range(n):
+    # Loop over each Node in the graph
+    for node in a:
         print("Starting for :",node)
         #If its not visited the run DFS to capture its whole structure and increment count
         if visited[node] == False:
             count+=1
             dfs(node,count,components)
     return count,components
+
 def dfs(at,count,components):
-    if visited[at] == True : return
-    visited[at] = True
-    neighbours = a[at]
-    components[at] = count
+    if visited[at] == True : return # Return if the node is alreday visited
+    visited[at] = True  #Else set  the nodes as visited
+    neighbours = a[at]  #Get the neighbours of the node
+    components[at] = count  #Set the component number for the node
     print(" ",at)
 
     #Loop over all the neighbours of the node
